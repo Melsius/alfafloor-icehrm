07dbfeb458833fae409be66d977d15ce48246c36
Mels <mels.van.broekhoven@gmail.com>
2019-12-17 20:50:37 +0100

    web/admin/src/electricity/lib.js: KWh -> kWh

--- web/admin/src/electricity/lib.js
+++ web/admin/src/electricity/lib.js
@@ -20,7 +20,7 @@ class ElectricityDashboardAdapter extends AdapterBase {
       { sTitle: 'ID', bVisible: false },
       { sTitle: 'Employee' },
       { sTitle: 'Date' },
-      { sTitle: 'Measurement (KWh)' },
+      { sTitle: 'Measurement (kWh)' },
       { sTitle: 'Details' },
     ];
   }
@@ -30,7 +30,7 @@ class ElectricityDashboardAdapter extends AdapterBase {
       ['id', { label: 'ID', type: 'hidden' }],
       ['employee', { label: 'Employee', type: 'select2', 'remote-source': ['Employee', 'id', 'first_name+last_name'] }],
       ['date', { label: 'Date', type: 'date', validation: ''}],
-      ['measurement', { label: 'Measurement (KWh)', type: 'text', validation: 'float' }],
+      ['measurement', { label: 'Measurement (kWh)', type: 'text', validation: 'float' }],
       ['details', { label: 'Details', type: 'textarea', validation: 'none' }],
     ];
   }
